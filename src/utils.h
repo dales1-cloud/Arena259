fdd2761d316ffaa3ed8028bc2842cffd532224e0
grumpywizardstudent <klein@sou.edu>
2026-04-21 15:34:00 -0700

    Adding color operations to utils.h for colorizing strings for linux terminal.

--- src/utils.h
+++ src/utils.h
@@ -3,16 +3,6 @@
 
 #include <iostream>
 #include <string>
-#include <random>
-
-static std::random_device RANDOM_DEVICE; 
-static std::mt19937 yield(RANDOM_DEVICE()); 
-static std::uniform_int_distribution<> COIN(0, 1);
-
-
-bool flipCoin() {
-    return COIN(yield);
-}
 
 enum class Color {
     RESET = 0,
@@ -32,22 +22,6 @@ std::ostream& operator<<(std::ostream& os, Color color) {
 
 std::string colorMe(std::string text, Color color) {
     return "\033[" + std::to_string(static_cast<int>(color)) + "m" + text + "\033[0m";
-}
-
-Color colorPicker() {
-    static const Color colors[] = {
-        Color::RED,
-        Color::GREEN,
-        Color::YELLOW,
-        Color::BLUE,
-        Color::MAGENTA,
-        Color::CYAN,
-    };
-
-    static const int count = sizeof(colors) / sizeof(colors[0]);
-
-    std::uniform_int_distribution<> dist(0, count - 1);
-    return colors[dist(yield)];
 }
 
 #endif
